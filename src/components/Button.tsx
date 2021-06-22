800f42e72abb5adc46739306e4ef698e9c2731a6
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-06-22 19:16:55 -0300

    Add Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import { ButtonHTMLAttributes } from 'react'
+import './styles.scss'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = (props: ButtonProps) => {
+	return (
+		<button className="button" { ...props }/>
+	)
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,38 @@
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+import { Button } from "../components/Button";
+import "../styles/auth.scss";
 
-import "../styles/auth.scss"
 
 export const Home = () => {
-	return (
-		<div className="page-auth">
-			<aside>
-				<img src={illustrationImg} alt="Illustration" />
-				<strong>Create Q&amp;A rooms</strong>
-				<p>learn and share knowledge with other people</p>
-			</aside>
-			<main className="main-content">
-				<div>
-					<img src={logoImg} alt="Let me asl logo" />
-					<button>
-						<img src={googleIconImg} alt="Google Icom" />
-						Create your room with Google
-					</button>
-					<div>ou entre em uma sala</div>
-					<form>
-						<input 
-							type="text" 
-							placeholder="Enter room code"
-						/>
-						<button type="submit">Enter the room</button>
-					</form>
-				</div>
-			</main>
-		</div>
-	)
-}
- 
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img
+          src={illustrationImg}
+          alt='Ilustração simbolizando perguntas e respostas'
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div className='main-content'>
+          <img src={logoImg} alt='Letmeask' />
+          <button className='create-room'>
+            <img src={googleIconImg} alt='Logo do Google' />
+            Crie sua sala com o Google
+          </button>
+
+          <div className='separator'>ou entre em uma sala</div>
+
+          <form action=''>
+            <input type='text' placeholder='Digite o código da sala' />
+            <Button type='submit'>Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
