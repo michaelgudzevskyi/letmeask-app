68a75a90f9f7d6f3eb628ef8fff9c92c3bfed088
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-06-23 20:55:57 -0300

    Create Data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AuthContextProvider } from './contexts/AuthContext';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <AuthContextProvider>
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
       </AuthContextProvider>
     </BrowserRouter>
   );
