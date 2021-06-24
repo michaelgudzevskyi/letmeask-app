25f75fccad2803bb1d53927dd255646a4ec31531
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-06-24 19:26:30 -0300

    Create Room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
 
 export const Room = () => {
-    return (
-        <div>
-            Room
-        </div>
-    )
-}
+
+
+  return (
+		<div id='page-room'>
+			<header>
+				<div className="content">
+					<img src={logoImg} alt="Let me ask" />
+
+					<div></div>
+				</div>
+			</header>
+
+			<main className="content">
+				<div className="room-title">
+					<h1>Room React</h1>
+					<span>4 questions</span>
+				</div>
+
+				<form>
+					<textarea 
+						placeholder="What do you want to ask?"
+					/>
+
+					<div className="form-footer">
+						<span>
+							To ask your question, you have to be 
+							<button type="button">authorized.</button>
+						</span>
+
+						<Button type="submit">Send your question</Button>
+					</div>
+				</form>
+			</main>
+		</div>
+	);
+};
