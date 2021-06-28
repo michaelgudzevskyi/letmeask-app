44e47610bea83e2a254761dec6068b648f5aff09
Mykhailo Hudzevatyi <thiagoduzzi@Mikes-MacBook-Pro.local>
2021-06-28 19:56:18 -0300

    Create Question

---
+++ src/components/Question.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+type QuestionProps = {
+  content: string
+  author: {
+    name: string
+    avatar: string
+  }
+}
+
+export const Question = ({ content, author }: QuestionProps) => {
+  return (
+    <div className="question">
+      <p>{content}</p>
+
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt="Author Name" />
+          <span>{author.name}</span>
+        </div>
+        <div></div>
+      </footer>
+    </div>
+  )
+}
